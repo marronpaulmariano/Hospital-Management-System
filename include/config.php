18e0e5983b8710b222a049b382cda47f19172d8f
marronpaulmariano <73495831+marronpaulmariano@users.noreply.github.com>
2023-03-12 04:00:49 -0700

    Update config.php

--- include/config.php
+++ include/config.php
@@ -2,7 +2,7 @@
 define('DB_SERVER','sql12.freemysqlhosting.net');
 define('DB_USER','sql12604989');
 define('DB_PASS' ,'qvTMXAPbM8');
-define('DB_NAME', 'sql12604989');
+define('DB_NAME','sql12604989');
 $con = mysqli_connect(DB_SERVER,DB_USER,DB_PASS,DB_NAME);
 // Check connection
 if (mysqli_connect_errno())
